b8df0b90c84d1d448d7639ad4800cf5b475eb27a
raor7192 <ynqkj092@ybb.ne.jp>
2014-03-28 21:20:55 +0900

    Update sidebar.js
    てすとん

--- WebContent/sidebar.js
+++ WebContent/sidebar.js
@@ -1,10 +1,9 @@
 
 function sidewidth(obj,towidth){
 	var element=document.getElementById("sidebar")
-	var style=getComputedStyle(element, '');
-	
-	
-}
+    element.style.width="120px";
+	}
+
 function ExtractNum( str ){
     var num = new String( str ).match(/\d/g);
     num = num.join("");
