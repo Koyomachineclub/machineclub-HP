f4df2317b40caad2e688d17fcf578f2e8b13b0ca
jp3bgy <heb52204@hcc5.bai.ne.jp>
2014-03-28 14:21:37 +0900

    変更なし

--- WebContent/sidebar.js
+++ WebContent/sidebar.js
@@ -3,6 +3,7 @@ function sidewidth(obj,towidth){
 	var element=document.getElementById("sidebar")
 	var style=getComputedStyle(element, '');
 	
+	
 }
 function ExtractNum( str ){
     var num = new String( str ).match(/\d/g);
