a4f4876a5fa545d200c140305c16c39ca51535a4
takamatu <takamatu@takamatu-System-Product-Name>
2014-03-29 21:23:37 +0900

    サムネイルをクリックすると画像が変わります

--- WebContent/viewphoto.js
+++ WebContent/viewphoto.js
@@ -1,19 +1,21 @@
 var photos=document.getElementsByClassName("viewphoto");
 var i;
+var active=1;
 function load(){
-	TimeoutID=setTimeout("viewphoto(1)",8000);
+	TimeoutID=setTimeout("viewphoto(2)",2000);
 }
 function viewphoto(photonumber){
-	//
-	i=photonumber
+	i=photonumber;
+	if(active===i)return;
 	clearTimeout(TimeoutID);
-	photo=document.getElementById("viewphoto"+i);
-	i++;
-	if(i>photos.length)i=1;
+	photo=document.getElementById("viewphoto"+active);
 	nextphoto=document.getElementById("viewphoto"+i);
 	photo.style.opacity=0;
 	photo.style.zIndex=0;
 	nextphoto.style.zIndex=1;
 	nextphoto.style.opacity=1;
-	TimeoutID=setTimeout("viewphoto(i)",8000);
+	active=i;
+	i++;
+	if(i>photos.length)i=1;
+	TimeoutID=setTimeout("viewphoto(i)",2000);
 }
