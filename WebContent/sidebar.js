7fa578f36d8b68e118d069a7e81f1982247225fd
takamatu <takamatu@takamatu-System-Product-Name>
2014-04-01 14:12:20 +0900

    連続クリックによるバグの修正

--- WebContent/sidebar.js
+++ WebContent/sidebar.js
@@ -1,5 +1,20 @@
+var totyu="0"; //連続クリックによるバグの修正
 $(".secmenu").css("display","none");
+$("#navi").click(function(){
+	if(totyu!=="0")return;
+	totyu="1";
+	if($("#sidebar").width()===60){
+		$("#sidebar").animate({width:'184px'},{duration:'slow',queue: false}).animate({height:'210px'},{duration:'slow',complete: function(){$("#sidebar").css("height","auto");totyu="0";}});
+	}else{
+		$(".secmenu:lt(8)").slideUp(500);
+		$(".secmenu:gt(8)").slideUp(500);
+		$("#sidebar").animate({width:'60px'},{duration:'slow',queue: false}).animate({height:'25px'},{duration:'slow',complete: function(){totyu="0";}});
+	}
+	return;
+});
 $("#one").click(function(){
+	if(totyu!=="0")return;
+	totyu="1";
 	if("none"===$("#onetwo").css("display")){
 		$("#oneone").css("display","none");
 		$("#onetwo").css("display","block");
@@ -7,9 +22,14 @@ $("#one").click(function(){
 		$("#oneone").css("display","block");
 		$("#onetwo").css("display","none");
 	}
-	$(".secmenu:lt(8)").slideToggle(500);
+	$(".secmenu:lt(8)").slideToggle(500,function() {
+		totyu="0";
+	  });
+	return;
 });
 $("#two").click(function(){
+	if(totyu!=="0")return;
+	totyu="1";
 	if("none"===$("#twotwo").css("display")){
 		$("#twoone").css("display","none");
 		$("#twotwo").css("display","block");
@@ -17,5 +37,8 @@ $("#two").click(function(){
 		$("#twoone").css("display","block");
 		$("#twotwo").css("display","none");
 	}
-	$(".secmenu:gt(8)").slideToggle(500);
+	$(".secmenu:gt(8)").slideToggle(500,function() {
+		totyu="0";
+	  });
+	return;
 });
