9351b5e21a08c9490ad1ddf93e305c47d149f05c
takamatu <takamatu@takamatu-System-Product-Name>
2014-04-03 00:19:44 +0900

    ちょいと修正

--- WebContent/top.js
+++ WebContent/top.js
@@ -2,9 +2,10 @@ var imgwidth,imgheight,titlelength,per;
 
 var an=function(){
 	titlelength=$(".viewtitle").length;
-	per=100/titlelength;
-	$(".viewtitle").css("width",per+"%");
 	imgwidth=$("#img").width();
+	per=imgwidth/titlelength-1;
+	$(".viewtitle").css("width",per+"px");
+	$(".viewtitle:eq(2)").css("width",(per+1)+"px");
 	imgheight=imgwidth/8*5;
 	$("#img").css("height",imgheight+"px");	
 	$(".viewtitle").css("line-height",$(".viewtitle").height()+"px");
