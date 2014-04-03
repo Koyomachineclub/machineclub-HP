2f451c91d99e2dc7a747b0257a03f112d739409e
takamatu <takamatu@takamatu-System-Product-Name>
2014-04-03 11:06:39 +0900

    スマートフォンにちょっと対応

--- WebContent/top.js
+++ WebContent/top.js
@@ -3,10 +3,22 @@ var imgwidth,imgheight,titlelength,per;
 var an=function(){
 	titlelength=$(".viewtitle").length;
 	imgwidth=$("#img").width();
-	per=imgwidth/titlelength-1;
+	if(imgwidth>600){
+		imgheight=imgwidth/8*5;
+		per=imgwidth/titlelength-1;
+		$("#viewtitles").css("white-space","nowrap");
+		$("#view").css("height","90%");
+		$(".viewphoto").css("height","90%");
+	}else{
+		imgheight=imgwidth/4*3+2;
+		per=imgwidth;
+		$("#viewtitles").css("white-space","normal");
+		$("#view").css("height","70%");
+		$(".viewphoto").css("height","70%");
+	}
+	
 	$(".viewtitle").css("width",per+"px");
 	$(".viewtitle:eq(2)").css("width",(per+1)+"px");
-	imgheight=imgwidth/8*5;
 	$("#img").css("height",imgheight+"px");	
 	$(".viewtitle").css("line-height",$(".viewtitle").height()+"px");
 	console.log("a");
