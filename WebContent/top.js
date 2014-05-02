095c90597efad6509d6023de3acc8657d9a160a1
takamatsu <takamatsu@takamatsu-System-Product-Name>
2014-05-02 17:39:59 +0900

    viewアニメーション

--- WebContent/top.js
+++ WebContent/top.js
@@ -5,23 +5,24 @@ function an(){
 	imgwidth=$("#img").width();
 	if(imgwidth>640){
 		imgheight=imgwidth/8*5;
-		per=imgwidth/titlelength-1;
+		per=imgwidth/titlelength-5;
 		$("#viewtitles").css("white-space","nowrap");
 		$("#view").css("height","90%");
 		$(".viewphoto").css("height","90%");
+                $(".viewtitle").height(imgheight/10-2);
 	}else{
 		imgheight=imgwidth/4*3+2;
-		per=imgwidth;
+		per=imgwidth-4;
 		$("#viewtitles").css("white-space","normal");
 		$("#view").css("height","70%");
 		$(".viewphoto").css("height","70%");
+                $(".viewtitle").height(imgheight/10-2);
 	}
 	
 	$(".viewtitle").css("width",per+"px");
 	$(".viewtitle:eq(2)").css("width",(per+1)+"px");
 	$("#img").css("height",imgheight+"px");	
 	$(".viewtitle").css("line-height",$(".viewtitle").height()+"px");
-	console.log("a");
 }
 function block(){
   if(imgwidth===960){
